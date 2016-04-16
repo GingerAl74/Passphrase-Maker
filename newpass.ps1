739bc5dfcbb5e858c50f96aeff1758bf9002134c
GingerAl74 <alexbrannick@yahoo.com>
2016-04-16 11:31:09 -0400

    Update newpass.ps1

--- newpass.ps1
+++ newpass.ps1
@@ -1,21 +1,26 @@
 $len = read-host "How many words do you want to use in your passphrase? Select 1-10"
 $list = read-host "What is the full path and filename of your wordlist?"
+$meas = Get-Content $list | Measure-Object -Word | Select-Object -ExpandProperty Words
 $counter = 0
 do
 {
 $counter ++
 $pass = Get-Content $list
-$num = Get-Random -Maximum 101
+$num = Get-Random -Maximum $meas
 $word1 = $pass[$num]
-out-file -filepath fullpass.txt -encoding ascii -append -force -InputObject "$word1"  
+out-file -Filepath fullpass.txt -encoding ascii -append -force -InputObject "$word1"
 }
 while ($counter -lt $len)
-$full = Get-Content fullpass.txt
 
-write-host
-write-host
-Write-Host "Your new passwords is $full"
+Get-Content fullpass.txt| foreach {
+ $out = $out + $_
+}
+$out| Out-File fullpass2.txt
+
+$full = Get-Content fullpass2.txt
+
+write-host "your passphrase is $full"
 Remove-Item fullpass.txt
-write-host
-write-host
-Read-Host "Press ENTER when your password has been recorded"
+Remove-Item fullpass2.txt
+
+read-host "Press ENTER when your password has been recorded"
