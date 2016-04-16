66b222a33e6330d84a792e066c9663c5ab43bd86
GingerAl74 <alexbrannick@yahoo.com>
2016-04-15 20:02:54 -0400

    Create newpass.ps1

---
+++ newpass.ps1
@@ -0,0 +1,21 @@
+$len = read-host "How many words do you want to use in your passphrase? Select 1-10"
+$list = read-host "What is the full path and filename of your wordlist?"
+$counter = 0
+do
+{
+$counter ++
+$pass = Get-Content $list
+$num = Get-Random -Maximum 101
+$word1 = $pass[$num]
+out-file -literalpath fullpass.txt -encoding ascii -append -force -InputObject "$word1" -nonewline  
+}
+while ($counter -le $len)
+$full = Get-Content fullpass.txt
+
+write-host
+write-host
+Write-Host "Your new passwords is $full"
+Remove-Item fullpass.txt
+write-host
+write-host
+Read-Host "Press ENTER when your password has been recorded"
